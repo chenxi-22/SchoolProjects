ec323a68af113fd47096a4111873c87fe1b5b14c
griezmanna8 <793650121@qq.com>
2019-04-09 18:05:59 +0800

    fix: projectname

--- QuestionnaireSystem/src/main/java/root/model/Teacher.java
+++ QuestionnaireSystem/src/main/java/root/model/Teacher.java
@@ -10,19 +10,15 @@ public class Teacher extends User {
         public int _noCount;
     }
 
-    //问卷，其实就是封装了String数组，数组下标表示问题编号
-    private class Naire{
-        public String _quest
-    }
     private Map<String, String[]> _questionnaireMap;   //key为课程，value为问卷
     private static Map<String, ResCount[]> _res_count_map;    //key为科目，value为每个问题的统计结果，数组下标表示问题编号
 
     public Teacher(){}
 
     public boolean AddQuestion(String subject, String question){
-
+        return true;
     }
     public boolean DelQuestion(String subject, String question){
-
+return true;
     }
 }
