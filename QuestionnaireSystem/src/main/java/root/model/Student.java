ec7ba0c5b9a3f3454dd1efa40f8138e1173e42a4
griezmanna8 <793650121@qq.com>
2019-04-10 18:35:32 +0800

    "fix:get and set"

--- QuestionnaireSystem/src/main/java/root/model/Student.java
+++ QuestionnaireSystem/src/main/java/root/model/Student.java
@@ -1,31 +1,34 @@
 package root.model;
 
-
 import java.util.List;
 import java.util.Vector;
 
 public class Student{
-    /**
-     * 学号，密码
-     */
     private String id;
     private String password;
-    /**
-     * 学生已选科目,String方便数据库操作
-     * List<String>方便外部操作
-     */
-    private String subjectsStr;
-    private List<String> subjectsList = new Vector<String>();
+    private List<String> subjects = new Vector<>();
 
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
-    public String getPWD() { return password; }
-    public void setPWD(String password) { this.password = password; }
-    public String getHasSubject() { return subjectsStr; }
-    public void setHasSubject(String subjectsStr) { this.subjectsStr = subjectsStr; }
+    public String getId() {
+        return id;
+    }
 
-    public List<String> getSubjectsList() { return subjectsList; }
+    public void setId(String id) {
+        this.id = id;
+    }
 
-    public void setSubjectsList(List<String> subjectsList) { this.subjectsList = subjectsList; }
+    public String getPassword() {
+        return password;
+    }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<String> getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(List<String> subjects) {
+        this.subjects= subjects;
+    }
 }
