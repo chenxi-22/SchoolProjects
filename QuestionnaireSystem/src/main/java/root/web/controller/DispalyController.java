78a5e9be76043716b3c6901ffc1872a2389aea14
griezmanna8 <793650121@qq.com>
2019-04-11 02:49:07 +0800

    "add controller"

--- QuestionnaireSystem/src/main/java/root/web/controller/DispalyController.java
+++ QuestionnaireSystem/src/main/java/root/web/controller/DispalyController.java
@@ -24,17 +24,68 @@ public class DispalyController {
     @RequestMapping(value = {"/"}, method ={RequestMethod.GET})
     public ModelAndView Home() {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("home");
+        modelAndView.setViewName("login");
         return modelAndView;
     }
 
     /**
      * 登录操作处理
+     * TODO
      * @return
      */
     @RequestMapping(value = {"/login"}, method = {RequestMethod.POST})
     public ModelAndView Login(@RequestParam(value = "id") String id, @RequestParam(value = "password") String password) {
         ModelAndView modelAndView = new ModelAndView();
+        /**
+         * 如果登录成功，判断该用户是学生还是老师，根据不同的身份，来返回不同的界面
+         * 如果该用户是学生，那么这里返回 student.jsp，如果是老师返回 teacher.jsp
+         */
         return modelAndView;
     }
+
+    /**
+     * 添加选课信息
+     * TODO
+     * @return
+     */
+    @RequestMapping(value = {"/student_add"}, method = {RequestMethod.GET})
+    public ModelAndView StudentAddSubject() {
+        ModelAndView modelAndView = new ModelAndView();
+        return modelAndView;
+    }
+
+    /**
+     * 浏览问卷
+     * TODO
+     * @return
+     */
+    @RequestMapping(value = {"/student_watch"}, method = {RequestMethod.GET})
+    public ModelAndView StudentWatch() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("student_watch");
+        return modelAndView;
+    }
+
+    /**
+     * 做问卷，采用 POST 方式提交问卷的结果
+     * TODO
+     * @return
+     */
+    @RequestMapping(value = {"/student_do"}, method = {RequestMethod.POST})
+    public ModelAndView StudentDo() {
+        ModelAndView modelAndView = new ModelAndView();
+        return modelAndView;
+    }
+
+    /**
+     * 查看问卷结果
+     * TODO
+     * @return
+     */
+    @RequestMapping(value = {"/student_get_result"}, method = {RequestMethod.GET})
+    public ModelAndView StudentGetResult() {
+        ModelAndView modelAndView = new ModelAndView();
+        return modelAndView;
+    }
+
 }
